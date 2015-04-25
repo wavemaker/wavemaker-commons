1d9bc7501e169a560c7a70c83551f7b6859960c9
Venugopal Jidigam <venugopal.jidigam@wavemaker.com>
2015-04-25 11:07:46 +0530

    adding additiona overload methods to JSONutils
    Change-Id: I768b47cf834bf1284ed116c86a82b34009420e86

--- wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/json/JSONUtils.java
+++ wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/json/JSONUtils.java
@@ -1,9 +1,6 @@
 package com.wavemaker.studio.common.json;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wavemaker.studio.common.util.IOUtils;
@@ -21,6 +18,11 @@ public class JSONUtils {
     public static void toJSON(File outputFile, Object object) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(outputFile, object);
+    }
+
+    public static void toJSON(OutputStream outputStream, Object object) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, object);
     }
 
     public static <T> T toObject(String jsonString, Class<T> t) throws IOException {
