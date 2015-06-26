6b1494e41a23b83be04f06ec6567df3fd4639ce6
Uday Shankar <uday.adepu@wavemaker.com>
2015-06-26 06:20:47 +0530

    Added utitily class which can read and write to props file
    Change-Id: I49cfc91a928596b24155fa8749a93568fc53caf3

---
+++ wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/util/PropertiesFileUtils.java
@@ -0,0 +1,46 @@
+package com.wavemaker.studio.common.util;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+import com.wavemaker.studio.common.WMRuntimeException;
+
+/**
+ * @author Uday Shankar
+ */
+public class PropertiesFileUtils {
+
+    public static Properties loadFromXml(File file) {
+        try {
+            return loadFromXml(new BufferedInputStream(new FileInputStream(file)));
+        } catch (FileNotFoundException e) {
+            throw new WMRuntimeException("File:" + file.getAbsolutePath() + " not found", e);
+        }
+    }
+
+    public static Properties loadFromXml(InputStream is) {
+        try {
+            Properties properties = new Properties();
+            properties.loadFromXML(is);
+            return properties;
+        } catch (IOException e) {
+            throw new WMRuntimeException("Failed to read properties input stream", e);
+        } finally {
+            IOUtils.closeSilently(is);
+        }
+    }
+
+    public static void storeToXml(Properties properties, OutputStream os, String comment) {
+        try {
+            properties.storeToXML(os, comment, "UTF-8");
+        } catch (IOException e) {
+            throw new WMRuntimeException("Failed to write properties file to output stream", e);
+        }
+    }
+}
