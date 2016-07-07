f2b23391d0b973936279fac81614d2da8194457b
Kishore Routhu <kishore.routhu@wavemaker.com>
2016-07-07 15:20:16 +0530

    Added XSSConfig and XSSFilterStrategy
    Change-Id: Iab4f118625cb3ab91455bc190e3487a3e3799365

---
+++ wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/model/security/XSSConfig.java
@@ -0,0 +1,53 @@
+package com.wavemaker.studio.common.model.security;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by kishorer on 6/7/16.
+ */
+public class XSSConfig {
+
+    private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTION)$");
+
+    private boolean enforceXssSecurity;
+    private String policyFile;
+    private XSSFilterStrategy xssFilterStrategy;
+
+    public boolean isEnforceXssSecurity() {
+        return enforceXssSecurity;
+    }
+
+    public void setEnforceXssSecurity(final boolean enforceXssSecurity) {
+        this.enforceXssSecurity = enforceXssSecurity;
+    }
+
+    public String getPolicyFile() {
+        return policyFile;
+    }
+
+    public void setPolicyFile(final String policyFile) {
+        this.policyFile = policyFile;
+    }
+
+    public Pattern getAllowedMethods() {
+        return allowedMethods;
+    }
+
+    public XSSFilterStrategy getXssFilterStrategy() {
+        return xssFilterStrategy;
+    }
+
+    public void setXssFilterStrategy(final XSSFilterStrategy xssFilterStrategy) {
+        this.xssFilterStrategy = xssFilterStrategy;
+    }
+
+    @Override
+    public String toString() {
+        return "XSSConfig{" +
+                "allowedMethods=" + allowedMethods +
+                ", enforceXssSecurity=" + enforceXssSecurity +
+                ", policyFile='" + policyFile + '\'' +
+                ", xssFilterStrategy=" + xssFilterStrategy +
+                '}';
+    }
+}
---
+++ wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/model/security/XSSFilterStrategy.java
@@ -0,0 +1,12 @@
+package com.wavemaker.studio.common.model.security;
+
+/**
+ * Created by kishorer on 29/6/16.
+ */
+public enum XSSFilterStrategy {
+
+    NONE,
+    ENCODE,
+    WHITE_LIST
+
+}
