52e58a26bb9518c01db815d05fddcd5ab73f2618
Saddham Pathan <saddham.pathan@wavemaker.com>
2016-07-12 13:55:11 +0530

    Created LocalTime serializer and deserializer
    Change-Id: I88ce613378599c27f2f0e34e5fdad200b7ce4c23

---
+++ wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/json/deserializer/WMLocalTimeDeserializer.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2016 WaveMaker, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.wavemaker.studio.common.json.deserializer;
+
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.wavemaker.studio.common.WMRuntimeException;
+
+/**
+ * Created by saddhamp on 11/7/16.
+ */
+public class WMLocalTimeDeserializer extends JsonDeserializer<LocalTime>{
+
+    private final static DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("HH:mm:ss");
+
+    @Override
+    public LocalTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+        JsonToken currentToken = jsonParser.getCurrentToken();
+        if (currentToken == JsonToken.VALUE_STRING) {
+            String value = jsonParser.getText();
+            return getLocalDateTime(value);
+        }
+        throw new WMRuntimeException("Not a String value");
+    }
+
+    public static LocalTime getLocalDateTime(String value) {
+        if (StringUtils.isBlank(value)) {
+            return null;
+        }
+        return dateTimeFormatter.parseLocalTime(value);
+    }
+}
+
