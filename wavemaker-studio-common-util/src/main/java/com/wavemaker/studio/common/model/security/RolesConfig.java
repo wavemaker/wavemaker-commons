475a8ad917dc8124ef832705bb515541049ebdde
Uday Shankar <uday.adepu@wavemaker.com>
2016-11-22 10:42:38 +0530

    created RolesConfig  which maintains a map of role name vs roleConfig
    Change-Id: If6e3c84bb80f659486690f9d8a3eb8c078550576

---
+++ wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/model/security/RolesConfig.java
@@ -0,0 +1,34 @@
+package com.wavemaker.studio.common.model.security;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Uday Shankar
+ */
+public class RolesConfig {
+
+    private Map<String, RoleConfig> roleMap;
+
+    public Map<String, RoleConfig> getRoleMap() {
+        return roleMap;
+    }
+
+    public void setRoleMap(Map<String, RoleConfig> roleMap) {
+        this.roleMap = roleMap;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RolesConfig that = (RolesConfig) o;
+        return Objects.equals(roleMap, that.roleMap);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleMap);
+    }
+
+}
