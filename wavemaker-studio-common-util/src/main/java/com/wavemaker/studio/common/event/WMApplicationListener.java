c931775bb3985349394e1ddb6330aedb159722ba
Kishore Routhu <kishore.routhu@wavemaker.com>
2016-11-23 19:13:09 +0530

    Fixing event delegation issue with listeners
    Change-Id: I4e5ba4095531e89de869eb91c1f89e42935b9f1e

--- wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/event/WMApplicationListener.java
+++ wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/event/WMApplicationListener.java
@@ -1,5 +1,6 @@
 package com.wavemaker.studio.common.event;
 
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.annotation.Order;
 
@@ -7,5 +8,5 @@ import org.springframework.core.annotation.Order;
  * @author Uday Shankar
  */
 @Order(value = 0)
-public interface WMApplicationListener<T extends WMEvent> extends ApplicationListener<T> {
+public interface WMApplicationListener<E extends ApplicationEvent> extends ApplicationListener<E> {
 }
