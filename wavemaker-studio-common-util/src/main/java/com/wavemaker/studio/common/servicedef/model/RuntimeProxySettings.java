16c9de7a43d53e7c6224fb55f51aab0ee76766c6
Venkateswarlu <venkateswarlu.katta@wavemaker.com>
2016-12-05 14:41:11 +0530

    Added equals and hashcode methods in RuntimeProxySettings model
    Change-Id: I63b4486847899bb7ab1652fe9ada56878b961133

--- wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/servicedef/model/RuntimeProxySettings.java
+++ wavemaker-studio-common-util/src/main/java/com/wavemaker/studio/common/servicedef/model/RuntimeProxySettings.java
@@ -31,4 +31,31 @@ public class RuntimeProxySettings {
     public void setMobile(boolean mobile) {
         this.mobile = mobile;
     }
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final RuntimeProxySettings that = (RuntimeProxySettings) o;
+
+        if (web != that.web) return false;
+        return mobile == that.mobile;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (web ? 1 : 0);
+        result = 31 * result + (mobile ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "RuntimeProxySettings{" +
+                "web=" + web +
+                ", mobile=" + mobile +
+                '}';
+    }
+
 }
