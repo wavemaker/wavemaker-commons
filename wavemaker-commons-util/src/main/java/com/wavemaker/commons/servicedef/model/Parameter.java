cd1e23e45c011779b8c3322b63d24afed0bab8a9
Srujan Thammisetty <srujan.thammisetty@wavemaker.com>
2017-02-08 16:44:25 +0530

    multipart/form-data: Added new property 'contentType' to specify mediatype of requestParts
    Change-Id: Id158fe3bed4fa33a24fe2c3f7b71a145cbd6fa03

--- wavemaker-commons-util/src/main/java/com/wavemaker/commons/servicedef/model/Parameter.java
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/servicedef/model/Parameter.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2013 - 2017 WaveMaker, Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,6 +30,7 @@ public class Parameter {
     private String type;
     private boolean required;
     private List<String> requiredFields;
+    private String contentType;
 
     @JsonIgnore
     public static synchronized Parameter getNewInstance() {
@@ -80,6 +81,7 @@ public class Parameter {
         return this;
     }
 
+
     public boolean isRequired() {
         return required;
     }
@@ -101,6 +103,20 @@ public class Parameter {
         return this;
     }
 
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public Parameter addContentType(final String contentType) {
+        this.contentType = contentType;
+        return this;
+    }
+
+
     @Override
     public String toString() {
         return "Parameter{" +
@@ -108,6 +124,7 @@ public class Parameter {
                 ", parameterType='" + parameterType + '\'' +
                 ", type='" + type + '\'' +
                 ", required=" + required +
+                ", contentType='" + contentType + '\'' +
                 '}';
     }
 
