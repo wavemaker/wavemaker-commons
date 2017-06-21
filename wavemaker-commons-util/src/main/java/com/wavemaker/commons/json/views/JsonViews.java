6b98f82bac91654f8db2b5486d43933797fdde0e
Dilip Kumar <dilip.gundu@wavemaker.com>
2017-06-21 12:38:40 +0530

    0# Introduced JsonView's for serializing blob fields as String only from executing Query controller.
    Change-Id: I9eaba7f3a7903a5680d8336bbd2f6df9d3537589

---
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/json/views/JsonViews.java
@@ -0,0 +1,11 @@
+package com.wavemaker.commons.json.views;
+
+/**
+ * @author <a href="mailto:dilip.gundu@wavemaker.com">Dilip Kumar</a>
+ * @since 21/6/17
+ */
+public class JsonViews {
+
+    public interface BlobAsUrlView {
+    }
+}
