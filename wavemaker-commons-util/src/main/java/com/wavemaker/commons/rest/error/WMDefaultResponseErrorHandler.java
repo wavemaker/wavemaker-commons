8fa6cfaa556615df94bf306564c37a2dbe3f65cb
Uday Shankar <uday.adepu@wavemaker.com>
2017-07-18 16:07:15 +0530

    Added WMDefaultResponseErrorHandler which logs in error handler if unable to read response stream
    Change-Id: Icc175fe722b1e29946d6853824f66bbe67090957

---
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/rest/error/WMDefaultResponseErrorHandler.java
@@ -0,0 +1,34 @@
+package com.wavemaker.commons.rest.error;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+
+import com.wavemaker.commons.util.StringUtils;
+
+/**
+ * @author Uday Shankar
+ */
+public class WMDefaultResponseErrorHandler extends DefaultResponseErrorHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(WMDefaultResponseErrorHandler.class);
+
+    @Override
+    protected byte[] getResponseBody(ClientHttpResponse response) {
+        InputStream inputStream = null;
+        try {
+            inputStream = response.getBody();
+            return IOUtils.toByteArray(inputStream);
+        } catch (IOException e) {
+            logger.error("Failed to read error response", e);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+        return new byte[0];
+    }
+}
