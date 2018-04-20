2fea31edfcb72618d9f009a287167968c1b24aa1
Srujan Thammisetty <srujan.thammisetty@wavemaker.com>
2018-04-20 09:34:51 +0530

    WorkSpaceSyncPlugin: Added new ErrorResponseWrapper class.
    Change-Id: I16d9cd34c3a525c7780960d17b5641db89bc764c

---
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/core/web/rest/ErrorResponsesWrapper.java
@@ -0,0 +1,13 @@
+package com.wavemaker.commons.core.web.rest;
+
+public class ErrorResponsesWrapper {
+    private ErrorResponses errors;
+
+    public ErrorResponses getErrors() {
+        return errors;
+    }
+
+    public void setErrors(ErrorResponses errors ) {
+        this.errors = errors;
+    }
+}
