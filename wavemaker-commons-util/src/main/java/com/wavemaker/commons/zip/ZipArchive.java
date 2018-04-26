9c76a79858134accbb260cad9220cb51d6313df7
Srujan Thammisetty <srujan.thammisetty@wavemaker.com>
2018-04-26 09:00:04 +0530

    WorkSpaceSyncPlugin: Added code to compress folder to zipFile.
    Change-Id: Ib3b64592b295fdf60900b67bef01fb3ced207e16

--- wavemaker-commons-util/src/main/java/com/wavemaker/commons/zip/ZipArchive.java
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/zip/ZipArchive.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 WaveMaker, Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -96,13 +96,18 @@ public class ZipArchive extends StoredFolder {
         }
     }
 
+    public static void compress(Folder folder, File file) {
+        InputStream inputStream = compress(folder);
+        file.getContent().write(inputStream);
+    }
+
     /**
      * Create a {@link InputStream} containing a zip representation of the given folder.
      *
      * @param folder the folder to compress
      */
     public static InputStream compress(Folder folder) {
-        return compress(folder, null);
+        return compress(folder, (String) null);
     }
 
     /**
