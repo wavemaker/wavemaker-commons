1df5e27281a9fe0dc71b2d5e5103236c964ef266
Prakash_B <prakash.buthukuri@wavemaker.com>
2018-06-21 21:15:04 +0530

    ArtifactsPublish : Added new method to get tempFile.
    Change-Id: Ibe525af1da26a9029f62c1887b332f1ec0f716bc

--- wavemaker-commons-util/src/main/java/com/wavemaker/commons/io/DeleteTempFileOnCloseInputStream.java
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/io/DeleteTempFileOnCloseInputStream.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -54,6 +54,10 @@ public class DeleteTempFileOnCloseInputStream extends FileInputStream {
     protected void finalize() throws IOException {
         deleteTempFile();
         super.finalize();
+    }
+
+    public File getTempFile() {
+        return tempFile;
     }
 
     private void deleteTempFile() {
