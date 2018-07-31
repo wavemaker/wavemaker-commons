2057bdf56d4ac4828e3511a9986d2296a73d5102
Srujan Thammisetty <srujan.thammisetty@wavemaker.com>
2018-07-31 20:16:00 +0530

    OpenID: Added OpenIdProvider model.
    Change-Id: I847ca4aa1c296cda7040f9a19527ed6ce33f92e6

---
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/auth/openId/OpenIdProviderInfo.java
@@ -0,0 +1,100 @@
+package com.wavemaker.commons.auth.openId;
+
+
+import java.util.List;
+
+/**
+ * Created by srujant on 31/7/18.
+ */
+public class OpenIdProviderInfo {
+
+    private String providerId;
+    private String clientId;
+    private String clientSecret;
+    private String authorizationUrl;
+    private String tokenUrl;
+    private String jwkSetUrl;
+    private String userInfoUrl;
+    private List<String> scopes;
+    private String redirectUrlTemplate;
+
+
+    public OpenIdProviderInfo() {
+    }
+
+    public OpenIdProviderInfo(String providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public String getAuthorizationUrl() {
+        return authorizationUrl;
+    }
+
+    public void setAuthorizationUrl(String authorizationUrl) {
+        this.authorizationUrl = authorizationUrl;
+    }
+
+    public String getTokenUrl() {
+        return tokenUrl;
+    }
+
+    public void setTokenUrl(String tokenUrl) {
+        this.tokenUrl = tokenUrl;
+    }
+
+    public String getJwkSetUrl() {
+        return jwkSetUrl;
+    }
+
+    public void setJwkSetUrl(String jwkSetUrl) {
+        this.jwkSetUrl = jwkSetUrl;
+    }
+
+    public String getUserInfoUrl() {
+        return userInfoUrl;
+    }
+
+    public void setUserInfoUrl(String userInfoUrl) {
+        this.userInfoUrl = userInfoUrl;
+    }
+
+    public List<String> getScopes() {
+        return scopes;
+    }
+
+    public void setScopes(List<String> scopes) {
+        this.scopes = scopes;
+    }
+
+    public String getRedirectUrlTemplate() {
+        return redirectUrlTemplate;
+    }
+
+    public void setRedirectUrlTemplate(String redirectUrlTemplate) {
+        this.redirectUrlTemplate = redirectUrlTemplate;
+    }
+}
