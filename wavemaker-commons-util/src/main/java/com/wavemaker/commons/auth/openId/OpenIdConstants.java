b4f9d4544f68cc77ecbfa263ebc80ea00dc81539
Srujan Thammisetty <srujan.thammisetty@wavemaker.com>
2018-08-08 07:49:38 +0530

    OpenID: Added OpenID constants.
    Change-Id: I6db6468c80de9941c234c38ef3296cdffb66e14c

--- wavemaker-commons-util/src/main/java/com/wavemaker/commons/auth/openId/OpenIdConstants.java
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/auth/openId/OpenIdConstants.java
@@ -9,4 +9,25 @@ public class OpenIdConstants {
     public static final String REGISTRATION_ID_URI_VARIABLE_NAME = "registrationId";
     public static final String APP_PATH = "appPath";
 
+    public static final String RESPONSE_TYPE = "response_type";
+
+    public static final String CLIENT_ID = "client_id";
+
+    public static final String REDIRECT_URI = "redirect_uri";
+
+    public static final String SCOPE = "scope";
+
+    public static final String STATE = "state";
+
+    public static final String CODE = "code";
+
+    public static final String ERROR = "error";
+
+    public static final String ERROR_DESCRIPTION = "error_description";
+
+    public static final String ERROR_URI = "error_uri";
+
+    public static final String REGISTRATION_ID = "registration_id";
+
+
 }
