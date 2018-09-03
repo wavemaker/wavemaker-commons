ece45b98a471f19bdcd41d28c98f79a7dfc208bb
rabindra <rabindra.patra@wavemaker.com>
2018-09-03 19:51:02 +0530

    pattern matching replace reader
    Change-Id: I6805e790dba82057836a7f2306ca6565184f90e6

---
+++ wavemaker-commons-util/src/test/java/com/wavemaker/commons/util/utils/PatternMatchingReplaceReaderTest.java
@@ -0,0 +1,73 @@
+package com.wavemaker.commons.util.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.StandardEnvironment;
+
+import com.wavemaker.commons.util.PatternMatchingReplaceReader;
+
+public class PatternMatchingReplaceReaderTest {
+
+
+    private static Environment env = null;
+
+    @BeforeClass
+    public static void beforeClass() {
+        env = new StandardEnvironment();// after deployment in tomcat it takes StandardServletEnvironment();
+    }
+
+    @Test
+    public void testWithSimpleTextFile() throws IOException {
+        String fileName = "pattern-matching-reader/pattern-file1.txt";
+        System.setProperty("rest.googleapis.basepath", "my-app-path");
+        System.setProperty("rest.googleapis.scheme","my-app-scheme");
+        System.setProperty("rest.google-app_store.drive","google-drive");
+
+        String resolvedString = getResolvedStringForFile(fileName, "${{", "}}");
+        String expectedOutput = IOUtils.resourceToString("pattern-matching-reader/expected-file1.txt", Charset.forName("UTF-8"), PatternMatchingReplaceReaderTest.class.getClassLoader());
+        Assert.assertEquals(expectedOutput, resolvedString);
+    }
+
+    @Test
+    public void testWithXmlFile() throws IOException
+    {
+        String fileName= "pattern-matching-reader/pattern-file2.xml";
+        System.setProperty("jdbc.driver","driver1");
+        System.setProperty("jdbc.db-user_name","user1");
+        System.setProperty("jdbc.db-password_1","pass123");
+        System.setProperty("app.class","com.java.Person");
+        System.setProperty("app.name","Person Info");
+        System.setProperty("app.version","19.23");
+
+        String resolvedString=getResolvedStringForFile(fileName, "${", "}");
+        String expectedOutput = IOUtils.resourceToString("pattern-matching-reader/expected-file2.xml", Charset.forName("UTF-8"), PatternMatchingReplaceReaderTest.class.getClassLoader());
+        Assert.assertEquals(expectedOutput, resolvedString);
+    }
+
+    private String getResolvedStringForFile(String fileName, String prefix, String suffix) throws IOException {
+        Reader pmr = null;
+        try {
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(fileName);
+            Reader isr = new InputStreamReader(is);
+            pmr = new PatternMatchingReplaceReader(isr, prefix, suffix, key -> env.getProperty(key));
+
+            StringWriter stringWriter = new StringWriter();
+            IOUtils.copy(pmr, stringWriter);
+            return stringWriter.toString();
+        } finally {
+            if (pmr != null) {
+                pmr.close();
+            }
+        }
+    }
+}
