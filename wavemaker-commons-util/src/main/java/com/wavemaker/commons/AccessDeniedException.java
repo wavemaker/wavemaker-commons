719cbdb01b414991e1f4db1fbf428e6980acf942
Srujan Thammisetty <srujan.thammisetty@wavemaker.com>
2018-09-25 20:19:33 +0530

    Added AccessDeniedException.
    Change-Id: I31a3a31c58fe9a6fb213f579edbc0ec5e1ba220a
    (cherry picked from commit 69ed0b8c63e87a813790b9075ca18cf78086802b)

---
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/AccessDeniedException.java
@@ -0,0 +1,9 @@
+package com.wavemaker.commons;
+
+/**
+ * Created by srujant on 25/9/18.
+ */
+public class AccessDeniedException extends WMRuntimeException {
+    public AccessDeniedException() {
+    }
+}
