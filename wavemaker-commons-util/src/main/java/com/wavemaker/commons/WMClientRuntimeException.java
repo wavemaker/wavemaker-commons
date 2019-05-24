bb292c886b4010fbf4a2742f745cd56ca48223a3
Kothapalli Ramesh Varma <ramesh.kothapalli@wavemaker.com>
2019-05-24 11:57:51 +0530

    removing WMClientRuntimeException class
    Change-Id: Ib7607eb8332e97da6c5dc7e679e219eefdfd4c4e

--- wavemaker-commons-util/src/main/java/com/wavemaker/commons/WMClientRuntimeException.java
+++
@@ -1,9 +0,0 @@
-package com.wavemaker.commons;
-
-public class WMClientRuntimeException extends WMRuntimeException {
-
-    public WMClientRuntimeException(MessageResource resource, Object... args) {
-        super(resource, args);
-    }
-
-}
