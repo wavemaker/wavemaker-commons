0a2aed19c2e0519b498e8268080791be7bcc10e6
Uday Shankar <uday.adepu@wavemaker.com>
2020-09-09 13:03:59 +0530

    Updated license header for java files
    Change-Id: Id3a5e6a3b5d85387ef00414fb33ffdb9f5fff99b

--- wavemaker-commons-util/src/main/java/com/wavemaker/commons/zip/ZipArchive.java
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/zip/ZipArchive.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2015 WaveMaker, Inc.
+ * Copyright (C) 2020 WaveMaker, Inc.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
