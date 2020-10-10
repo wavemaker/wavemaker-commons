a7ef27c9ad6119b24a09a1331f4c803808d51df3
Uday Shankar <uday.adepu@wavemaker.com>
2020-10-10 09:06:55 +0530

    Publishing the maven artifacts using the custom precompile plugin java-library-maven-publish and java-platform-maven-plugin

--- wavemaker-commons.gradle.kts
+++ wavemaker-commons.gradle.kts
@@ -20,8 +20,7 @@ buildscript {
 }
 
 plugins {
-    `java-platform`
-    `maven-publish`
+    `java-platform-maven-publish`
 }
 
 group ="com.wavemaker.commons"
@@ -51,12 +50,6 @@ dependencies {
     }
 }
 
-publishing {
-    configurePublicationToDist(this)
-    publications {
-        create<MavenPublication>("maven") {
-            artifactId = project.extensions.extraProperties.get("basename") as String
-            from(components["javaPlatform"])
-        }
-    }
+javaPlatformMavenPublish {
+    scmUrl="git:https://github.com/wavemaker/wavemaker-commons.git"
 }
