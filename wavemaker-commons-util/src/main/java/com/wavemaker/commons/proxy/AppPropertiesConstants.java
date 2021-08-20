0a17a7c43c19b531b5e2ff33f62b9072b47bc9bd
Sanjana <sanjana.raheja@wavemaker.com>
2021-08-20 15:29:35 +0530

    WMS-21065: Update the build profile values in the profile properties file and update key in build.xml file.

--- wavemaker-commons-util/src/main/java/com/wavemaker/commons/proxy/AppPropertiesConstants.java
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/proxy/AppPropertiesConstants.java
@@ -34,7 +34,7 @@ public final class AppPropertiesConstants {
     public static final String APP_COMPRESSION_EXCLUDE_TYPES = getAppPropertyKey("web.compression.mimetypes.excludePatterns");
 
     public static final String APP_BUILD_UI_MODE = getAppPropertyKey("build.ui.mode");
-    public static final String APP_BUILD_UI_NG_ARGS = getAppPropertyKey("build.ui.ng.args");
+    public static final String APP_BUILD_UI_NG_CONFIG = getAppPropertyKey("build.ui.ng.config");
 
     private static String getAppPropertyKey(String key) {
         return new StringBuilder(APP_PREFIX).append(".").append(key).toString();
