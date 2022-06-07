bd4a1f4f26baef8034694a327b8c9c614bf99f35
Kothapalli Ramesh Varma <ramesh.kothapalli@wavemaker.com>
2022-06-07 14:21:00 +0530

    WMS-22346: casting to BeanSerializerBase

--- wavemaker-commons-util/src/main/java/com/wavemaker/commons/json/module/WMBeanSerializerModifier.java
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/json/module/WMBeanSerializerModifier.java
@@ -18,8 +18,8 @@ package com.wavemaker.commons.json.module;
 import com.fasterxml.jackson.databind.BeanDescription;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializationConfig;
-import com.fasterxml.jackson.databind.ser.BeanSerializer;
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+import com.fasterxml.jackson.databind.ser.std.BeanSerializerBase;
 
 /**
  * @author <a href="mailto:dilip.gundu@wavemaker.com">Dilip Kumar</a>
@@ -36,8 +36,8 @@ public class WMBeanSerializerModifier extends BeanSerializerModifier {
     @Override
     public JsonSerializer<?> modifySerializer(
             final SerializationConfig config, final BeanDescription beanDesc, final JsonSerializer<?> serializer) {
-        if (serializer instanceof BeanSerializer) {
-            return new CircularLoopHandlingSerializer<>((BeanSerializer) serializer,
+        if (serializer instanceof BeanSerializerBase) {
+            return new CircularLoopHandlingSerializer<>((BeanSerializerBase) serializer,
                     module.isFailOnCircularReferences());
         } else {
             return serializer;
