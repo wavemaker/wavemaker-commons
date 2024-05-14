3e53ab1c5359c1e61e612e2e96764b47ad5ffbb3
Abhitej25 <abhitej.konkimalla@wavemaker.com>
2024-05-14 17:32:49 +0530

    WMS-25698 stack upgrade

--- wavemaker-commons-util/src/main/java/com/wavemaker/commons/util/CoreFilterUtil.java
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/util/CoreFilterUtil.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import com.wavemaker.commons.pattern.URLPattern;
 
