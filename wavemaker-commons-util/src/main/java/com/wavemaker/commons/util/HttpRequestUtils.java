ac83d655f65c031c939aee2c2accd9d03559ecd4
Abhitej25 <abhitej.konkimalla@wavemaker.com>
2025-03-26 12:37:29 +0530

    WMS-27856 Refactored FrameOptions Filter, removed the IEBrowser support for frameOptions

--- wavemaker-commons-util/src/main/java/com/wavemaker/commons/util/HttpRequestUtils.java
+++ wavemaker-commons-util/src/main/java/com/wavemaker/commons/util/HttpRequestUtils.java
@@ -115,15 +115,6 @@ public class HttpRequestUtils {
         return sb.toString();
     }
 
-    public static boolean isRequestedFromIEBrowser(HttpServletRequest httpServletRequest) {
-        String userAgent = httpServletRequest.getHeader("User-Agent");
-        /*
-         * Latest versions of IE(from IE11) doesn't include 'MSIE' header. We depend on revisionVersion(rv) and layout engine("Trident") properties, to verify
-         * if the request is from IE.
-         * */
-        return (userAgent != null && (userAgent.contains("MSIE") || (userAgent.contains("rv") && userAgent.contains("Trident"))));
-    }
-
     public static String getApplicationBaseUrl(HttpServletRequest httpServletRequest) {
         return new StringBuilder(getBaseUrl(httpServletRequest)).append(httpServletRequest.getContextPath()).toString();
     }
